e7649c515bc4470e16613f0aadc04a37d95d6ce6
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-07-22 02:49:01 +0100

    formatação do output na consola e troca da API

--- src/App.java
+++ src/App.java
@@ -5,14 +5,21 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.io.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
 
+    // declarar o ANSI_RESET de forma a fazer o reset de uma cor escolhida
+    public static final String ANSI_RESET = "\u001B[0m";
+    // declarar a cor do background
+    public static final String PURPLE_BACKGROUND = "\u001B[45m";
+
+    public static void main(String[] args) throws Exception {
         // fazer uma conexão HTTP e obter o top 250 filmes
         // url alternativa
         //String url = "https://api.mocki.io/v2/549a5d8b";
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_d6o9vfgi";
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_d6o9vfgi";
+        String url = "https://imdb-api.com/en/API/MostPopularMovies/k_d6o9vfgi";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -27,7 +34,8 @@ public class App {
         for (Map<String,String> filme : listaDeFilmes) {
             System.out.println(filme.get("title"));
             System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            System.out.println(PURPLE_BACKGROUND + "Classificação: " + filme.get("imDbRating") + ANSI_RESET);
+            System.out.println("teste" + " \2B50");
             System.out.println();
             
         }

62d6ecebbb729fe4ab10f3bdd8a8f4f4f80cad31
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-07-22 01:58:44 +0100

    forma de extração dos dados e criação do parser

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -16,9 +18,11 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
-
+        
         // extrair os dados que interessam (título, imagem, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
 
         // apresentar e manipular os dados 
     }

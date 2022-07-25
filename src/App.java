d6c090e85b7e6fe95fb43830326b1ae1b98e2555
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-07-25 23:48:36 +0100

    Correção do ciclo for para 4 imagens = inter data

--- src/App.java
+++ src/App.java
@@ -35,7 +35,7 @@ public class App {
         // apresentar e manipular os dados 
         var fabrica = new fabricaDeStickers();
         
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 4; i++) {
         
             Map<String,String> filme = listaDeFilmes.get(i);
 
